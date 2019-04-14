c5310a0a13ac91df9d83ff72dcd4e0b51e482d19
Michael Liao <askxuefeng@gmail.com>
2019-04-14 15:54:23 +0800

    fix null for Map

--- src/main/java/com/itranswarp/common/ApiException.java
+++ src/main/java/com/itranswarp/common/ApiException.java
@@ -1,5 +1,6 @@
 package com.itranswarp.common;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.itranswarp.enums.ApiError;
@@ -9,22 +10,26 @@ import com.itranswarp.enums.ApiError;
  */
 public class ApiException extends RuntimeException {
 
-	public final String error;
+	public final ApiError error;
 	public final String data;
 
 	public ApiException(ApiError error) {
 		super(error.name());
-		this.error = error.name();
+		this.error = error;
 		this.data = null;
 	}
 
 	public ApiException(ApiError error, String data, String message) {
 		super(message);
-		this.error = error.name();
+		this.error = error;
 		this.data = data;
 	}
 
 	public Map<String, String> toMap() {
-		return Map.of("error", this.error, "data", this.data, "message", this.getMessage());
+		Map<String, String> map = new HashMap<>();
+		map.put("error", this.error.name());
+		map.put("data", this.data);
+		map.put("message", this.getMessage());
+		return map;
 	}
 }

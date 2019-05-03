6383f040be82eac5dd24fb21ba56081be3c856cf
Michael Liao <askxuefeng@gmail.com>
2019-05-03 14:40:41 +0800

    add backup script

---
+++ script/backup_db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# backup database by exporting db as a single sql file
+
+# change the host, user, db if necessary:
+MYSQL_HOST=localhost
+MYSQL_USER=root
+MYSQL_DB=it
+
+# backup file name:
+BACKUP=it-`date +"%Y-%m-%d_%H_%M_%S"`.sql
+
+echo "mysqldump -h $MYSQL_HOST --opt --hex-blob --set-gtid-purged=OFF --user $MYSQL_USER -p $MYSQL_DB > $BACKUP"
+
+mysqldump -h $MYSQL_HOST --opt --hex-blob --set-gtid-purged=OFF --user $MYSQL_USER -p $MYSQL_DB > $BACKUP
+
+gzip $BACKUP
+
+echo "backup ok: $BACKUP.gz"

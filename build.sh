032ecc2c22608b0ba4c2fa41321a807d745aa19f
Michael Liao <askxuefeng@gmail.com>
2019-05-05 18:57:00 +0800

    add comment

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# set JDK 11, only works on Mac:
 export JAVA_HOME=`/usr/libexec/java_home -v 11`
 
+# maven build:
 mvn -DskipTests clean package
 
 cp target/itranswarp.jar release/
 
+# compress resources:
 cd src/main/resources
 tar --exclude ".*" -czvf ../../../release/resources.tar.gz favicon.ico robots.txt static/
 cd ../../..

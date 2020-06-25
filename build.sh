bff502f3b091c4ab6fa3a1176eca57449e455e49
Michael Liao <askxuefeng@gmail.com>
2020-06-25 09:14:55 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# set JDK 11, only works on Mac:
-export JAVA_HOME=`/usr/libexec/java_home -v 11`
+# check java version:
+JAVA_VERSION=`java -version 2>&1 | awk 'NR==1{ gsub(/"/,""); print $3 }'`
+
+if [[ $JAVA_VERSION == 11* ]]; then
+  echo "Java version ok: $JAVA_VERSION"
+else
+  echo "Java version is expected as 11 but $JAVA_VERSION. try:"
+  echo "export JAVA_HOME=\`/usr/libexec/java_home -v 11\`"
+  exit 1
+fi
 
 # maven build:
 ./mvnw -DskipTests clean package

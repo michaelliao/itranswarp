1e38f9be4ad4f2a34170354ad3fa85ffcc8f1169
Michael Liao <askxuefeng@gmail.com>
2022-04-20 20:25:17 +0800

    set runtime to jdk17

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ cd "$(dirname "$0")"
 # check java version:
 JAVA_VERSION=`java -version 2>&1 | awk 'NR==1{ gsub(/"/,""); print $3 }'`
 
-if [[ $JAVA_VERSION == 11* ]]; then
+if [[ $JAVA_VERSION == 17* ]]; then
   echo "Java version ok: $JAVA_VERSION"
 else
-  echo "Java version is expected as 11 but $JAVA_VERSION. try:"
-  echo "export JAVA_HOME=\`/usr/libexec/java_home -v 11\`"
+  echo "Java version is expected as 17 but $JAVA_VERSION. try:"
+  echo "export JAVA_HOME=\`/usr/libexec/java_home -v 17\`"
   exit 1
 fi
 

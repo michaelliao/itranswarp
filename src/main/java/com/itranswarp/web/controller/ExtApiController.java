2fa0c7cd6ca0990452f196b64f769f867f0b9bf7
Michael Liao <askxuefeng@gmail.com>
2023-03-28 08:37:14 +0800

    add chat-gpt support

--- src/main/java/com/itranswarp/web/controller/ExtApiController.java
+++ src/main/java/com/itranswarp/web/controller/ExtApiController.java
@@ -5,8 +5,13 @@ import java.net.http.HttpClient;
 import java.net.http.HttpClient.Version;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpRequest.Builder;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
@@ -28,6 +33,7 @@ import org.springframework.web.context.request.async.DeferredResult;
 import com.itranswarp.common.AbstractService;
 import com.itranswarp.common.ApiException;
 import com.itranswarp.enums.ApiError;
+import com.itranswarp.model.User;
 import com.itranswarp.redis.RedisService;
 import com.itranswarp.util.JsonUtil;
 import com.itranswarp.web.filter.HttpContext;
@@ -54,6 +60,27 @@ public class ExtApiController extends AbstractService {
     @Value("${spring.external.remote-code-runner.max-concurrent:10}")
     int remoteCodeRunnerMaxConcurrent;
 
+    @Value("${spring.external.chat-gpt.enabled:false}")
+    boolean chatGptEnabled;
+
+    @Value("${spring.external.chat-gpt.url:https://api.openai.com/v1/chat/completions}")
+    String chatGptUrl;
+
+    @Value("${spring.external.chat-gpt.timeout:30}")
+    int chatGptTimeout;
+
+    @Value("${spring.external.chat-gpt.api-key:}")
+    String chatGptApiKey;
+
+    @Value("${spring.external.chat-gpt.model:gpt-3.5-turbo}")
+    String chatGptModel;
+
+    @Value("${spring.external.chat-gpt.prompt:}")
+    String chatGptPrompt;
+
+    @Value("${spring.external.chat-gpt.rate-limit:100}")
+    long chatGptRateLimit;
+
     @Autowired
     RedisService redisService;
 
@@ -69,6 +96,61 @@ public class ExtApiController extends AbstractService {
                 .executor(executor).build();
     }
 
+    // call chat-gpt //////////////////////////////////////////////////////////
+
+    @PostMapping("/gpt")
+    @ResponseBody
+    public DeferredResult<String> chatGpt(@RequestBody ChatGptInput input) {
+        if (!this.chatGptEnabled) {
+            throw new ApiException(ApiError.OPERATION_FAILED, null, "ChatGPT service is not enabled.");
+        }
+        if (this.chatGptUrl == null || this.chatGptUrl.isEmpty()) {
+            throw new ApiException(ApiError.OPERATION_FAILED, null, "ChatGPT URL is not set.");
+        }
+        User user = HttpContext.getCurrentUser();
+        if (user == null) {
+            throw new ApiException(ApiError.AUTH_SIGNIN_REQUIRED, null, "Signin required.");
+        }
+        if (input.content == null || input.content.isBlank() || input.content.length() > 500) {
+            throw new ApiException(ApiError.PARAMETER_INVALID, "content", "Invalid content.");
+        }
+        // check rate limit:
+        String key = "_cgpt_" + LocalDate.now() + "_" + user.id;
+        long times = redisService.executeSync(command -> {
+            return command.incr(key);
+        });
+        if (times > this.chatGptRateLimit) {
+            throw new ApiException(ApiError.RATE_LIMIT, null, "Maximum reached.");
+        }
+        List<Map<String, String>> messages = new ArrayList<>(2);
+        if (this.chatGptPrompt != null && !this.chatGptPrompt.isBlank()) {
+            messages.add(Map.of("role", "system", "content", this.chatGptPrompt));
+        }
+        messages.add(Map.of("role", "user", "content", input.content.strip()));
+        Map<String, Object> inputData = Map.of("model", this.chatGptModel, "messages", messages);
+        byte[] jsonData = JsonUtil.writeJsonAsBytes(inputData);
+        Builder builder = HttpRequest.newBuilder(URI.create(this.chatGptUrl)) // url
+                .timeout(Duration.ofSeconds(this.chatGptTimeout)) // timeout
+                .header("Content-Type", "application/json"); // json
+        if (this.chatGptApiKey != null && !this.chatGptApiKey.isEmpty()) {
+            builder.header("Authorization", "Bearer " + this.chatGptApiKey); // auth
+        }
+        HttpRequest request = builder.POST(BodyPublishers.ofByteArray(jsonData)).version(Version.HTTP_1_1).build();
+        CompletableFuture<HttpResponse<String>> cf = this.httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        DeferredResult<String> dr = new DeferredResult<>(Long.valueOf(30_000), () -> {
+            return JsonUtil.writeJson(Map.of("error", ApiError.TIMEOUT));
+        });
+        cf.thenAccept(resp -> {
+            dr.setResult(resp.body());
+        });
+        cf.exceptionally(e -> {
+            logger.error("call chat gpt error!", e);
+            dr.setErrorResult(new ApiException(ApiError.INTERNAL_SERVER_ERROR));
+            return null;
+        });
+        return dr;
+    }
+
     // remote code running ////////////////////////////////////////////////////
 
     @PostMapping("/remoteCodeRun")
@@ -76,6 +158,10 @@ public class ExtApiController extends AbstractService {
     public DeferredResult<String> remoteCodeRun(@RequestBody RemoteCodeRunInput input) {
         if (!this.remoteCodeRunnerEnabled) {
             throw new ApiException(ApiError.OPERATION_FAILED, null, "Remote code running service is not enabled.");
+        }
+        User user = HttpContext.getCurrentUser();
+        if (user == null) {
+            throw new ApiException(ApiError.AUTH_SIGNIN_REQUIRED, null, "Signin required.");
         }
         if (input.language == null || !this.remoteCodeRunnerLanguages.contains(input.language)) {
             throw new ApiException(ApiError.PARAMETER_INVALID, "language", "Invalid language.");
@@ -121,6 +207,10 @@ public class ExtApiController extends AbstractService {
     }
 
     // static bean class //////////////////////////////////////////////////////
+
+    public static class ChatGptInput {
+        String content;
+    }
 
     public static class RemoteCodeRunInput {
         public String language;
